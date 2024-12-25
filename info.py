2ada83a4ea10c64e92855a872679fcb5bf20c4e2
primeyour <primeyourr@gmail.com>
2024-12-25 19:10:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://full-trixie-primeyour-13b7cc8d.koyeb.app/")
+URL = environ.get("URL", "")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
