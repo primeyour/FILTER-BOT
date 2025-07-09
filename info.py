872fbb733f9c3de3058212caf86193c22c50500c
primeyour <primeyourr@gmail.com>
2025-07-09 17:19:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,168 +1,157 @@
-# Don't Remove Credit @
+# Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @
-
+# Ask Doubt on telegram @KingVJ01
 
 import re
+import os
 from os import environ
 from Script import script 
+from dotenv import load_dotenv
 
+# Load environment variables from .env file if present
+load_dotenv()
+
+# Pattern for validating IDs
 id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
 
-# Bot information
+def is_enabled(value, default):
+    """Convert string values to boolean"""
+    if not value:
+        return default
+    return value.lower() in ("true", "yes", "1", "enable", "y")
+
+# ============================ Bot Information ============================
 SESSION = environ.get('SESSION', 'KeralaCaptain')
 API_ID = int(environ.get('API_ID', '20836266'))
 API_HASH = environ.get('API_HASH', 'bbdd206f92e1ca4bc4935b43dfd4a2a1')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
-# Bot settings
+# ============================ Bot Settings ===============================
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', "https://envs.sh/sxe.jpg")).split() #SAMPLE PIC
+PICS = (environ.get('PICS', "https://envs.sh/sxe.jpg")).split()
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
-# Admins, Channels & Users
+# ============================ Admins, Channels & Users ===================
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002462335914'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7577977996 6644681404').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
-# auth_channel means force subscribe channel.
-# if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
-REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', True)) # Set True Or False
-TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', True)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
-auth_channel = environ.get('AUTH_CHANNEL', '-1002449923339') # give your force subscribe channel id here else leave it blank
+# Force Subscribe Configuration
+REQUEST_TO_JOIN_MODE = is_enabled(environ.get('REQUEST_TO_JOIN_MODE', 'True'), True)
+TRY_AGAIN_BTN = is_enabled(environ.get('TRY_AGAIN_BTN', 'True'), True)
+auth_channel = environ.get('AUTH_CHANNEL', '-1002449923339')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+
+# Channel and Group IDs
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002493027541')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002306779893')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002405400154')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in environ.get('FILE_STORE_CHANNEL', '-1002405400154').split()]
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
-# MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://primemastix:o84aVniXFmKfyMwH@cluster0.qgiry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# ============================ Database Configuration =====================
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://primemastix:o84aVniXFmKfyMwH@cluster0.qgiry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "primemastix")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TomCollection')
 
-# Premium And Referal Settings
-PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', False)) # Set Ture Or False
-
-# If PREMIUM_AND_REFERAL_MODE is True Then Fill Below Variable, If Flase Then No Need To Fill.
-REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '20')) # number of referal count
+# ============================ Premium & Referral =========================
+PREMIUM_AND_REFERAL_MODE = is_enabled(environ.get('PREMIUM_AND_REFERAL_MODE', 'False'), False)
+REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '20'))
 REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1month')
 PAYMENT_QR = environ.get('PAYMENT_QR', '')
-PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟᴀɴs - \n\n- 30ʀs - 1 ᴡᴇᴇᴋ\n- 50ʀs - 1 ᴍᴏɴᴛʜs\n- 120ʀs - 3 ᴍᴏɴᴛʜs\n- 220ʀs - 6 ᴍᴏɴᴛʜs\n\n🎁 ᴘʀᴇᴍɪᴜᴍ ғᴇᴀᴛᴜʀᴇs 🎁\n\n○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴠᴇʀɪғʏ\n○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋ\n○ ᴅɪʀᴇᴄᴛ ғɪʟᴇs\n○ ᴀᴅ-ғʀᴇᴇ ᴇxᴘᴇʀɪᴇɴᴄᴇ\n○ ʜɪɢʜ-sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ\n○ ᴍᴜʟᴛɪ-ᴘʟᴀʏᴇʀ sᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋs\n○ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs\n○ ꜰᴜʟʟ ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ\n○ ʀᴇǫᴜᴇsᴛ ᴡɪʟʟ ʙᴇ ᴄᴏᴍᴘʟᴇᴛᴇᴅ ɪɴ 1ʜ ɪꜰ ᴀᴠᴀɪʟᴀʙʟᴇ\n\n✨ ᴜᴘɪ ɪᴅ - <code>jivshn@okaxis</code>\n\nᴄʟɪᴄᴋ ᴛᴏ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴀᴄᴛɪᴠᴇ ᴘʟᴀɴ /myplan\n\n💢 ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ\n\n‼️ ᴀғᴛᴇʀ sᴇɴᴅɪɴɢ ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴜs sᴏᴍᴇ ᴛɪᴍᴇ ᴛᴏ ᴀᴅᴅ ʏᴏᴜ ɪɴ ᴛʜᴇ ᴘʀᴇᴍɪᴜᴍ</b>')
-OWNER_USERNAME = environ.get('OWNER_USERNAME', 'CjjTom') # owner username without @
+PAYMENT_TEXT = environ.get('PAYMENT_TEXT', script.PAYMENT_TEXT)
+OWNER_USERNAME = environ.get('OWNER_USERNAME', 'CjjTom')
 
-# Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
-CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
-PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'KeralaPrimeMovie') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
+# ============================ Clone Mode Settings ========================
+CLONE_MODE = is_enabled(environ.get('CLONE_MODE', 'False'), False)
+CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "")
+PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'KeralaPrimeMovie')
 
-# Links
+# ============================ Links ======================================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/CJMovieSearch')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/CJMovieSearch')
 TUTORIAL = environ.get('TUTORIAL', '')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'KeralaLeechHelp') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'KeralaLeechHelp')
 
-# True Or False
-AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
-PM_SEARCH = bool(environ.get('PM_SEARCH', False))
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK',False))
-MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
-IMDB = is_enabled((environ.get('IMDB', "True")), True)
-AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
-AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
+# ============================ Feature Toggles ============================
+AI_SPELL_CHECK = is_enabled(environ.get('AI_SPELL_CHECK', 'True'), True)
+PM_SEARCH = is_enabled(environ.get('PM_SEARCH', 'False'), True)  # Fixed PM_SEARCH to True by default
+IS_SHORTLINK = is_enabled(environ.get('IS_SHORTLINK', 'False'), False)
+MAX_BTN = is_enabled(environ.get('MAX_BTN', 'True'), True)
+IS_TUTORIAL = is_enabled(environ.get('IS_TUTORIAL', 'True'), True)
+P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', 'False'), False)
+IMDB = is_enabled(environ.get('IMDB', 'True'), True)
+AUTO_FFILTER = is_enabled(environ.get('AUTO_FFILTER', 'True'), True)
+AUTO_DELETE = is_enabled(environ.get('AUTO_DELETE', 'True'), True)
+SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', 'True'), True)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "True"), True)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
-NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
+PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
+PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
+NO_RESULTS_MSG = is_enabled(environ.get("NO_RESULTS_MSG", "False"), False)
+USE_CAPTION_FILTER = is_enabled(environ.get('USE_CAPTION_FILTER', "True"), True)
 
-# Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
+# ============================ Verification Settings ======================
+VERIFY = is_enabled(environ.get('VERIFY', 'False'), False)
+VERIFY_SECOND_SHORTNER = is_enabled(environ.get('VERIFY_SECOND_SHORTNER', 'False'), False)
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', '')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
-# if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', '')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
-# Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
+# ============================ Shortlink Configuration ====================
+SHORTLINK_MODE = is_enabled(environ.get('SHORTLINK_MODE', 'False'), False)
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', '317e5f6ab1aa6fba5fb3623a24caf6a26f6e177e')
 
-# Others
-MAX_B_TN = environ.get("MAX_B_TN", "5")
+# ============================ Other Settings =============================
+MAX_B_TN = int(environ.get("MAX_B_TN", "5"))
 PORT = environ.get("PORT", "8080")
 MSG_ALRT = environ.get('MSG_ALRT', 'ʜɪ ʙʀᴏ ᴡʜᴀᴛ s ᴜᴘ')
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", script.CAPTION)
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE_TXT)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 
-# Choose Option Settings 
-LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
-SEASONS = ["season 1", "season 2", "season 3", "season 4", "season 5", "season 6", "season 7", "season 8", "season 9", "season 10"]
-EPISODES = ["E01", "E02", "E03", "E04", "E05", "E06", "E07", "E08", "E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16", "E17", "E18", "E19", "E20", "E21", "E22", "E23", "E24", "E25", "E26", "E27", "E28", "E29", "E30", "E31", "E32", "E33", "E34", "E35", "E36", "E37", "E38", "E39", "E40"]
+# ============================ Search Filters =============================
+LANGUAGES = ["malayalam", "mal", "tamil", "tam", "english", "eng", "hindi", "hin", 
+             "telugu", "tel", "kannada", "kan"]
+SEASONS = [f"season {i}" for i in range(1, 11)]
+EPISODES = [f"E{i:02d}" for i in range(1, 41)]
 QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
-YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
+YEARS = [str(year) for year in range(1900, 2026)]
 
-
-                           # Don't Remove Credit 
-                           # Subscribe YouTube Channel For Amazing Bot 
-                           # Ask Doubt on telegram @KeralaPrime
-
-
-# Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
-
-# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+# ============================ Stream Mode ================================
+STREAM_MODE = is_enabled(environ.get('STREAM_MODE', 'True'), True)
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
+ON_HEROKU = 'DYNO' in environ
 URL = environ.get("URL", "https://full-trixie-primeyour-13b7cc8d.koyeb.app/")
 
+# ============================ File Handling ==============================
+RENAME_MODE = is_enabled(environ.get('RENAME_MODE', 'True'), True)
+AUTO_APPROVE_MODE = is_enabled(environ.get('AUTO_APPROVE_MODE', 'True'), True)
 
-# Rename Info : If True Then Bot Rename File Else Not
-RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or False
+# ============================ Logging Configuration ======================
+LOG_STR = "Current Configuration:\n"
+LOG_STR += f"IMDB: {'Enabled' if IMDB else 'Disabled'}\n"
+LOG_STR += f"P_TTI_SHOW_OFF: {'Enabled' if P_TTI_SHOW_OFF else 'Disabled'}\n"
+LOG_STR += f"SINGLE_BUTTON: {'Enabled' if SINGLE_BUTTON else 'Disabled'}\n"
+LOG_STR += f"CUSTOM_FILE_CAPTION: {CUSTOM_FILE_CAPTION[:50] + '...' if CUSTOM_FILE_CAPTION else 'Not Set'}\n"
+LOG_STR += f"LONG_IMDB_DESCRIPTION: {'Enabled' if LONG_IMDB_DESCRIPTION else 'Disabled'}\n"
+LOG_STR += f"SPELL_CHECK_REPLY: {'Enabled' if SPELL_CHECK_REPLY else 'Disabled'}\n"
+LOG_STR += f"MAX_LIST_ELM: {MAX_LIST_ELM if MAX_LIST_ELM else 'No Limit'}\n"
+LOG_STR += f"Current IMDB template:\n{IMDB_TEMPLATE[:200]}...\n" if IMDB_TEMPLATE else "No IMDB Template Set\n"
 
-# Auto Approve Info : If True Then Bot Approve New Upcoming Join Request Else Not
-AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE',True)) # Set True or False
-
-LOG_STR = "Current Cusomized Configurations are:-\n"
-LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
-LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
-LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
-LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
-LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
-LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
-LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
-LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
-
-
-# Don't Remove Credit 
-# Subscribe YouTube Channel For Amazing Bot @
-# Ask Doubt on telegram @KeralaPrime
+# Security check for sensitive information
+if any(cred in DATABASE_URI for cred in ['password', 'secret', 'token']):
+    logger.warning("Potential sensitive information detected in DATABASE_URI")
